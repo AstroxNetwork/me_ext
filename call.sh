54c8d4fcba60a9bbf575efcdcbaf62e89afdedc4
skyh24 <skyhighfeng@gmail.com>
2022-10-27 22:06:10 +0800

    mod script

--- call.sh
+++ call.sh
@@ -1,1 +1,3 @@
 dfx canister --network ic call me_ext getTokens 
+
+# dfx deploy --network ic me_ext -m reinstall --argument '(principal "gpnv5-a22hm-655yn-uqdyc-nux3k-76clz-t46di-6cz3j-ksayy-ym3tg-qae")'
